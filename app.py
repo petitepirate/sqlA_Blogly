00dc936b553c0ee4ab49ee1ef8639be274bf5c9f
megan <msmeganmcmanus@gmail.com>
2020-09-21 12:07:57 -0700

    finish project section 2

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 """Blogly application."""
 from flask_debugtoolbar import DebugToolbarExtension
 from flask import Flask, request, redirect, render_template
-from models import db, connect_db, User
+from models import db, connect_db, User, Post
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
@@ -20,6 +20,13 @@ debug = DebugToolbarExtension(app)
 # to be fixed later
 def home():
     return redirect("/users")
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    """Show 404 NOT FOUND page."""
+
+    return render_template('404.html'), 404
 
 
 @app.route("/users")
@@ -93,3 +100,69 @@ def delete_user(user_id):
     db.session.commit()
 
     return redirect("/users")
+###################################################################
+# Exercise 2 start
+
+
+@app.route("/users/<int:user_id>/posts/new")
+def new_post(user_id):
+    """Show edit form"""
+    user = User.query.get_or_404(user_id)
+    return render_template("newpostform.html", user=user)
+
+
+@app.route('/users/<int:user_id>/posts/new', methods=["POST"])
+def submit_post(user_id):
+    """new post"""
+    user = User.query.get_or_404(user_id)
+    new_post = Post(
+        title=request.form['title'],
+        content=request.form['content'], user=user
+    )
+
+    db.session.add(new_post)
+    db.session.commit()
+
+    return redirect(f"/users/{user_id}")
+
+
+@app.route('/posts/<int:post_id>')
+def posts_page(post_id):
+    """Page for each post"""
+
+    post = Post.query.get_or_404(post_id)
+    return render_template('postspage.html', post=post)
+
+
+@app.route('/posts/<int:post_id>/edit')
+def edit_post(post_id):
+    """Edit a post"""
+
+    post = Post.query.get_or_404(post_id)
+    return render_template('editpostform.html', post=post)
+
+
+@app.route('/posts/<int:post_id>/edit', methods=["POST"])
+def submit_post_edit(post_id):
+    """Handle form submission for updating an existing post"""
+
+    post = Post.query.get_or_404(post_id)
+    post.title = request.form['title']
+    post.content = request.form['content']
+
+    db.session.add(post)
+    db.session.commit()
+
+    return redirect(f"/users/{post.user_id}")
+
+
+@app.route('/posts/<int:post_id>/delete', methods=["POST"])
+def delete_post(post_id):
+    """Handle form submission for deleting an existing post"""
+
+    post = Post.query.get_or_404(post_id)
+
+    db.session.delete(post)
+    db.session.commit()
+
+    return redirect(f"/users/{post.user_id}")
